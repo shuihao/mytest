f2889d13235b443ddb93084056fae86383476516
shuihao <731233519@qq.com>
2019-07-24 17:21:57 +0800

    提交workspace.xml

--- untitled/src/com/test/Test.java
+++ untitled/src/com/test/Test.java
@@ -34,6 +34,6 @@ public class Test {
     String f = "1";
 
     public static void main(String[] args) {
-
+        System.out.println();
     }
 }
--- untitled/src/com/test/Test01.java
+++ untitled/src/com/test/Test01.java
@@ -1,4 +1,7 @@
 package com.test;
 
 public class Test01 {
+    private String name;
+    private String sex;
+    private String age;
 }
