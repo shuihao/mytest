c76486c086281d8a2cf83c3ee1cfc89bf3b95cca
shuihao <731233519@qq.com>
2019-07-24 13:37:44 +0800

    oooo

---
+++ untitled/src/com/test/Test.java
@@ -0,0 +1,39 @@
+package com.test;
+
+public class Test {
+
+    private String username;
+    private String password;
+
+    public Test(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    String a = "123";
+    String b = "123";
+    String c = "123";
+    String d = "123";
+
+    String f = "1";
+
+    public static void main(String[] args) {
+
+    }
+}
